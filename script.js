18fbe047b6a115754b738f9eb274f8cd4fb4e050
Ryan Dumlao <dlogsr@gmail.com>
2015-09-22 00:53:06 -0700

    waypoint for mid-face added, need to fix z-index to work with content div

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var windowHeight;
+
 function scrollAndStop(marker,offset){
 	var scrollOffset;
 	scrollOffset = $(marker).offset().top - offset;
@@ -9,15 +11,24 @@ function getWindowHeight(){
 }
 
 function adjustContentSpacing(currSection) {
-	var windowHeight = getWindowHeight();
-	$(currSection).css({'min-height':windowHeight-70});
+	windowHeight = getWindowHeight();
+	//$(currSection).css({'min-height':windowHeight-70});
+	//Specific to Headshot div only; dirty code
+	$('.head-div').css({'height':windowHeight/3});
 }
 
 function adjustContentOffset(currSection,num) {
-	var windowHeight = getWindowHeight();
+	windowHeight = getWindowHeight();
 	currSection.css({'top':windowHeight+num});
 }
 
 $(document).ready(function(){
 	adjustContentSpacing('.headshot');
+	$('.head-mid').css({'background-size': windowHeight});
+});
+
+$('.head-mid').waypoint(function(){
+	$('.headshot').addClass('headshot-fixed');
+	$('.headshot-fixed').css({top:-(windowHeight/3)});
+	$('.content').css({top:(windowHeight)});
 });
