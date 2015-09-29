87b5fb2f0a8e13b39798613648507f65fd3758a1
Ryan Dumlao <dlogsr@gmail.com>
2015-09-28 21:36:39 -0700

    fixed up-down scrolling numbering, added placeholder varidables for jQuery class callouts

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 $frameRight = $('.frame-right');
 $frameLeft = $('.frame-left');
-$headshotMid =  $('.headshot-mid');
 $midStreamLine = $('.mid-stream-line');
+$headshotMid = $('.headshot-mid');
+$borderLeftBrain = $('.border-left-brain');
+$borderRightBrain = $('.border-right-brain');
+$panelRight = $('.panel-right');
+$panelLeft = $('.panel-left');
+$lineRight = $('.line-right');
+$lineLeft = $('.line-left');
+$leftButton = $('.left-button');
+$rightButton = $('.right-button');
+$rightDown = $('.right-down');
+$rightUp = $('.right-up');
+$leftDown = $('.left-down');
+$leftUp = $('.left-up');
+$coverStreamLine = $('.cover-stream-line');
+$headDiv = $('.head-div');
+$brainButtons = $('.brain-buttons');
+$contentOverlay = $('.content-overlay');
+$contentOverlayPanel = $('.content-overlay-panel');
+$content = $('.content');
+$streamLine = $('.stream-line');
+$streamPhoto =$('.stream-photo');
 
 var windowHeight;
 var rightFrames = $frameRight.length;
@@ -16,6 +36,7 @@ function scrollToTop(time){
 
 function scrollAndStop(marker,offset,dir){
 	var frameMarker;
+	var $currMarker = $(marker);
 	(marker == '.frame-right') ? frameMarker = frameMarkerRight : frameMarker = frameMarkerLeft;
 
 	if(scrollOffset === undefined){
@@ -23,7 +44,6 @@ function scrollAndStop(marker,offset,dir){
 		frameMarker++;
 	}
 	else if(dir == 'down'){
-		var $currMarker = $(marker);
 		for(x = 0; x < frameMarker; x++){
 			$currMarker = $currMarker.next();
 		}
@@ -31,22 +51,20 @@ function scrollAndStop(marker,offset,dir){
 		frameMarker++;
 	}
 	else if (dir == 'up'){
-		if(frameMarker == 0){
+		if(frameMarker <= 1){
 			scrollOffset = 0;
 			frameMarkerRight = 0;
 			frameMarkerLeft = 0;
 			$midStreamLine.addClass('hide');
-			$('.panel-left').removeClass('on');
-			$('.panel-right').removeClass('on');
+			$panelLeft.removeClass('on');
+			$panelRight.removeClass('on');
 			$('.right-arrows .scroll-button').add($('.curve-left')).add($('.curve-right')).add($('.left-arrows .scroll-button')).add($('.right-brain-arrow')).add($('.left-brain-arrow')).addClass('hide');
-			// (marker == '.frame-right') ? $('.panel-left').addClass('on') : $('.panel-right').addClass('on');
-			//frameMarker++;
 		}
 		else{
-			if(frameMarker >= 0) frameMarker -=2;
+			if(frameMarker >= 0) frameMarker -=1;
 			if(frameMarker < 0) frameMarker = 0;	
 			var $currMarker = $(marker);
-			for(x = 0; x < frameMarker; x++){
+			for(x = 0; x < frameMarker-1; x++){
 				$currMarker = $currMarker.next();
 			}
 			scrollOffset = $currMarker.offset().top - offset;
@@ -70,13 +88,11 @@ function adjustContentSpacing(currSection) {
 	pageHeight = getPageHeight();
 	//$(currSection).css({'min-height':windowHeight-70});
 	//Specific to Headshot div only; dirty code
-	$('.head-div').add($headshotMid).add($midStreamLine).css({'height':windowHeight/3});
-	$('.brain-buttons').css({'height':windowHeight/3-8});
-	$('.cover-stream-line').add($('.stream-line')).add($('.content')).css({'height':pageHeight});
-	$('.content-overlay').add($('.content-overlay-panel')).css({'height':pageHeight});
-	// $('.stream-photo-frame').css({'max-height':windowHeight/3});
-	$('.stream-photo').css({'max-height':windowHeight/3.2, 'max-width':'100%'});
-	// $('.brain-arrow-curve').css({'height':windowHeight/3-46.867})
+	$headDiv.add($headshotMid).add($midStreamLine).css({'height':windowHeight/3});
+	$brainButtons.css({'height':windowHeight/3-8});
+	$coverStreamLine.add($streamLine).add($content).css({'height':pageHeight});
+	$contentOverlay.add($contentOverlayPanel).css({'height':pageHeight});
+	$streamPhoto.css({'max-height':windowHeight/3.2, 'max-width':'100%'});
 }
 
 function adjustContentOffset(currSection,num) {
@@ -88,74 +104,74 @@ $(document).ready(function(){
 	scrollToTop(0);
 	adjustContentSpacing('.headshot');
 	$headshotMid.css({'background-size': windowHeight});
-	$('.right-down').on('touchstart mousedown',function(e){
+	$rightDown.on('touchstart mousedown',function(e){
 		scrollAndStop('.frame-right',windowHeight/3,'down');
 	});
-	$('.right-up').on('touchstart mousedown',function(e){
+	$rightUp .on('touchstart mousedown',function(e){
 		scrollAndStop('.frame-right',windowHeight/3,'up');
 	});
-	$('.left-down').on('touchstart mousedown',function(e){
+	$leftDown.on('touchstart mousedown',function(e){
 		scrollAndStop('.frame-left',windowHeight/3,'down');
 	});
-	$('.left-up').on('touchstart mousedown',function(e){
+	$leftUp.on('touchstart mousedown',function(e){
 		scrollAndStop('.frame-left',windowHeight/3,'up');
 	});
 
-	$('.right-button').on('touchstart mousedown',function(e){
+	$rightButton.on('touchstart mousedown',function(e){
 		frameMarkerRight = 0;
 		scrollAndStop('.frame-right',windowHeight/3,'down');
 		setTimeout(function(){
 			$('.right-arrows .scroll-button').add($('.curve-right')).add($('.right-brain-arrow')).removeClass('hide');
 			$('.left-arrows .scroll-button').add($('.curve-left')).add($('.left-brain-arrow')).addClass('hide');
 			$midStreamLine.removeClass('hide');
-			$('.panel-left').addClass('on');
+			$panelLeft.addClass('on');
 			$('.frame-left .stream-photo-frame-inner').removeClass('border-left-brain');
 			$('.frame-right .stream-photo-frame-inner').addClass('border-right-brain');
-			$('.panel-right').removeClass('on');
-			$('.cover-stream-line').addClass('line-right').removeClass('line-left');
-			$('.headshot-mid').removeClass('border-left-brain').addClass('border-right-brain');
+			$panelRight.removeClass('on');
+			$coverStreamLine.addClass('line-right').removeClass('line-left');
+			$headshotMid.removeClass('border-left-brain').addClass('border-right-brain');
 		},400);
 	});
 
-	$('.left-button').on('touchstart mousedown',function(e){
+	$leftButton.on('touchstart mousedown',function(e){
 		frameMarkerLeft = 0;
 		scrollAndStop('.frame-left',windowHeight/3,'down');
 		setTimeout(function(){
 			$('.left-arrows .scroll-button').add($('.curve-left')).add($('.left-brain-arrow')).removeClass('hide');
 			$('.right-arrows .scroll-button').add($('.curve-right')).add($('.right-brain-arrow')).addClass('hide');
 			$midStreamLine.removeClass('hide');
-			$('.panel-left').removeClass('on');
-			$('.panel-right').addClass('on');
+			$panelLeft.removeClass('on');
+			$panelRight.addClass('on');
 			$('.frame-left .stream-photo-frame-inner').addClass('border-left-brain');
 			$('.frame-right .stream-photo-frame-inner').removeClass('border-right-brain');
-			$('.cover-stream-line').addClass('line-left').removeClass('line-right');
-			$('.headshot-mid').removeClass('border-right-brain').addClass('border-left-brain');
+			$coverStreamLine.addClass('line-left').removeClass('line-right');
+			$headshotMid.removeClass('border-right-brain').addClass('border-left-brain');
 		},400);
 	});
 
 });
 
 $('.head-mid').waypoint(function(direction){
-	$('.cover-stream-line').removeClass('hide');
+	$coverStreamLine.removeClass('hide');
 	$('.headshot-main').toggleClass('headshot-fixed');
 	$headshotMid.toggleClass('show');
 	if(direction=='down'){
 		$('.headshot-fixed').css({top:-(windowHeight/3)});
-		$('.content').css({top:(windowHeight)});
+		$content.css({top:(windowHeight)});
 	}
 	else{
-		$('.content').css({top:0});	
+		$content.css({top:0});	
 	}
 });
 
-$('.content').waypoint(function(direction){
+$content.waypoint(function(direction){
 	if(direction == 'down'){
-		$('.headshot-mid').addClass('bottom-border');
+		$headshotMid.addClass('bottom-border');
 	}
 	else{
-		$('.headshot-mid').removeClass('bottom-border');
-		// $('.panel-left').removeClass('on');
-		// $('.panel-right').removeClass('on');
+		$headshotMid.removeClass('bottom-border');
+		// $panelLeft.removeClass('on');
+		// $panelRight.removeClass('on');
 	}
 },{offset:'34%'});
 
